d55f4f287a696631e8e12ac296477da9a0cff55d
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-25 10:51:38 +0200

    Initializing the Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
+
   return (
     <main>
       <h1>Landing Page</h1>
+      {/* <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+      <Landing />
+      <Description />
+      <Projects />
+      <SlidingImages />
+      <Contact /> */}
     </main>
   );
 }
